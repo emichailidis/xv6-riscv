cbc330a104d27b149a865be6d936cefbe979864a
mariamiiiiii <maria-michailidi@outlook.com.gr>
2024-01-18 22:44:06 +0200

    after proc changes

--- kernel/proc.c
+++ kernel/proc.c
@@ -124,6 +124,8 @@ allocproc(void)
 found:
   p->pid = allocpid();
   p->state = USED;
+  p->priority=10;  //Maria default priority 
+  
 
   // Allocate a trapframe page.
   if((p->trapframe = (struct trapframe *)kalloc()) == 0){
